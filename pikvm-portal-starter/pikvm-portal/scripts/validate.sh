77586bccfc61c3b650646b03907d0c1e03ad66e0
perumrr <74992461+perumrr@users.noreply.github.com>
2026-09-11 14:05:27 -0400

    Delete pikvm-portal-starter/pikvm-portal directory

--- pikvm-portal-starter/pikvm-portal/scripts/validate.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-docker compose config
-docker compose run --rm authelia authelia config validate --config /config/configuration.yml
-echo "Basic configuration validation completed."
